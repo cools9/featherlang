7c1f12c73bc214d81518375a37508a32aa69a792
Cools9 <rishabh4a@christacademy.in>
2026-08-30 13:30:15 +0530

    broke a lot norhing works

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,10 +1,81 @@
 pub struct Lexer {
-    pub line: String,
+    pub source: Vec<char>,
+
+    pub tokens: Vec<TokenTypes>,
+    current_position: usize,
+    maybe_position: usize,
+}
+
+enum TokenTypes {
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    // One or two character tokens.
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    // Keywords.
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    EOF,
 }
 
 impl Lexer {
-    pub fn stripper(&mut self) -> Vec<&str> {
-        let words: Vec<&str> = self.line.split_whitespace().collect();
-        words
+    pub fn DetermineToken(&mut self) {
+        let character: char = self.source[self.current_position];
+        match character {
+            '{' => self.addToken(TokenTypes::LEFT_BRACE),
+            '}' => self.addToken(TokenTypes::RIGHT_BRACE),
+            '(' => self.addToken(TokenTypes::LEFT_PAREN),
+            ')' => self.addToken(TokenTypes::RIGHT_PAREN),
+            '+' => self.addToken(TokenTypes::PLUS),
+            '-' => self.addToken(TokenTypes::MINUS),
+            '*' => self.addToken(TokenTypes::STAR),
+            '.' => self.addToken(TokenTypes::DOT),
+            ';' => self.addToken(TokenTypes::SEMICOLON),
+            _ => todo!(),
+        }
+        self.advance();
+    }
+    pub fn addToken(&mut self, tokentype: TokenTypes) {
+        self.tokens.push(tokentype);
+    }
+    pub fn advance(&mut self) {
+        self.current_position += 1;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 mod lexer;
 use crate::lexer::lexer::Lexer;
+use clap::Parser;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file = File::open("main.fl")?;
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    filename: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    //println!("Hello {}!", args.name);
+    RunProgram(args.filename);
+}
+
+fn RunProgram(path: String) -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open(path)?;
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
