0856470cd1bf5de40567489e738203d6100231db
Cools9 <rishabh4a@christacademy.in>
2026-09-06 20:46:53 +0530

    ehh done a bit of work

--- src/ast/ast.rs
+++ src/ast/ast.rs
@@ -34,10 +34,44 @@ enum ast{
 
 }
 
-
+#[derive(Debug)]
 enum Operator {
     Add,
     Subtract,
     Multiply,
     Divide,
+}
+
+
+impl ast{
+    pub fn print(&self) {
+        match self {
+            ast::BinaryExpressions { left, right, operator } => {
+                left.print();
+                right.print();
+                println!("Operator: {:?}", operator);
+            }
+            ast::UnaryExpression { operator, operand } => {
+                operand.print();
+                println!("Operator: {:?}", operator);
+            }
+            ast::Let { name, value } => {
+                println!("Name: {}", name);
+                value.print();
+            }
+            ast::Print { value } => {
+                value.print();
+            }
+            ast::Number { value } => {
+                println!("Number: {}", value);
+            }
+            ast::String { value } => {
+                println!("String: {}", value);
+            }
+            ast::Bool { value } => {
+                println!("Bool: {}", value);
+            }
+        }
+    }
+
 }
